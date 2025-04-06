9502246546697d2ca0bfa02d9136263d88b8cddf
1975udit <udit2115418@akgec.ac.in>
2025-04-06 20:08:48 +0530

    bugs

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -11,7 +11,7 @@ const RootLayout =async ({children} : {children : ReactNode}) => {
   }
   return (
     <div className="root-layout">
-      <nav>
+      <nav> 
         <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
           <h2 className="text-primary-100">VoxCruit</h2>
